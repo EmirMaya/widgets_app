b6f7c17f3dd0f6f6a0f5ecf24010fd95275366d6
Emir Maya <emirmayaok@gmail.com>
2024-12-12 20:13:41 -0300

    screens para utilizar

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTutotialSreen extends StatelessWidget {
+  static const name = 'app_tutorial_screen';
+  
+  const AppTutotialSreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
